a5a254da5c90437fcbce9f922a89916daeda2055
waddewaa <wolovsson@gmail.com>
2020-10-09 20:37:38 +0200

    Now handles arguments. Main creates socket and binds to port. Ready for creating threads for each connection

--- db_server/src/main.c
+++ db_server/src/main.c
@@ -4,20 +4,76 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define PORT 2020
 
+void printHelpText();
 
 
 int main(int argc, char const *argv[]){
+    //Default port
+    int port = 2020;
+    //Get commandline arguments
+    for(int i = 1; i < argc; i++){
+        if(strcmp(argv[i], "-h") == 0){
+            printHelpText();
+            return 0;
+        }
+        if(strcmp(argv[i], "-p") == 0){
+            i++;
+            port = atoi(argv[i]);
+        }
+        else {
+            printf("%s not implemented\n", argv[i]);
+            printHelpText();
+            exit(3);
+        }
+    }
 
+    //Create the listening socket
+    int socket_fd;
+    struct sockaddr_in server_address;
+    //IPv4, TCP
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_fd < 0){
+        perror("Creation of server socket failed");
+        exit(1);
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    //Binding socket to port
+    if(bind(socket_fd, (struct sockaddr *) &server_address, sizeof(server_address))<0){
+        perror("Error on binding socket");
+        exit(1);
+    }
+    printf("Socket successfully bound to port %d\n", port);
+    listen(socket_fd,5);
+
+    //Listen for incoming connections, create thread for each connection
+    while(1){
+        int new_sock_fd;
+
+        if ((new_sock_fd = accept(socket_fd, (struct sockaddr *)&server_address,(socklen_t*)&server_address)) < 0){
+            perror("Failed to accept client");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    
+    return 0;
+
+
+
+    /*
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char str[1024] = {0};
     char *hello = "Hello from server";
-
+    printHelp();
 
     // Creating socker file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
@@ -61,12 +117,15 @@ int main(int argc, char const *argv[]){
     send(new_socket, hello, strlen(hello), 0);
     //printf("Hello message sent\n");
     }
-
+*/
     
+}
 
-
-
-
-
-    return 0;
+void printHelpText(){
+    printf("Lite SQL-server usage\n\n");
+    printf("\t-h\t\tPrints this help text\n");
+    printf("\t-p <port>\tListen to port number <port> (default:2020)\n");
+    printf("\t-d\t\tRun as deamon instead of as a normal program\n");
+    printf("\t-l <logfile>\tLog to logfile\n");
+    printf("\t-s [fork|thread|prefork|mux]\tSelect request handling method\n");
 }
